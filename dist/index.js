dbbd9632d08183358e421373b90d410f14aaf5e6
Michael Henderson <mhenderson@keyfactor.com>
2022-06-15 16:55:24 -0700

    add ncc compiled dist folder

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+const foo = 'bar'
+console.log(foo)
+module.exports = __webpack_exports__;
+/******/ })()
+;
