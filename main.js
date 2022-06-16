93892ac7d5f01fa2426c1e31433a2a922de3a5b1
Michael Henderson <mhenderson@keyfactor.com>
2022-06-15 17:13:58 -0700

    add git basic consts

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const foo = 'bar'
 console.log(foo)
+const core = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+
+const { owner, repo } = context.repo;
+const semver = require('semver');
+const process = require('process');
+
+const octokit = new GitHub(process.env.GITHUB_TOKEN);
